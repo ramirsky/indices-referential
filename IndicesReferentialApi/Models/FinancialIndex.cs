5d0218895ae50432dca96a2325746a2116d3c4ef
Ramiro Caio <ramiro.caio@gmail.com>
2022-07-27 19:19:22 +0200

    Update project

---
+++ IndicesReferentialApi/Models/FinancialIndex.cs
@@ -0,0 +1,12 @@
+﻿using System;
+namespace IndicesReferentialApi.Models
+{
+    public class FinancialIndex
+    {
+        public long Id { get; set; }
+        public string Name { get; set; }
+        public string Currency { get; set; }
+        public string ReturnType { get; set; }
+    }
+}
+
---
+++ IndicesReferentialApi/Models/FinancialIndexContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace IndicesReferentialApi.Models
+{
+    public class FinancialIndexContext : DbContext
+    {
+        public FinancialIndexContext(DbContextOptions<FinancialIndexContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<FinancialIndex> FinancialIndices { get; set; }
+    }
+}
+
