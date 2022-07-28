ab31358cec365db048cbb3ac5db0b110d6531e27
Ramiro Caio <ramiro.caio@gmail.com>
2022-07-28 17:40:12 +0200

    Update project

--- IndicesReferentialApi/Models/FinancialIndex.cs
+++ IndicesReferentialApi/Models/FinancialIndex.cs
@@ -1,8 +1,15 @@
-﻿using System;
-namespace IndicesReferentialApi.Models
+﻿namespace IndicesReferentialApi.Models
 {
     public class FinancialIndex
     {
+        public FinancialIndex(long id, string name, string currency, string returnType)
+        {
+            Id = id;
+            Name = name;
+            Currency = currency;
+            ReturnType = returnType;
+        }
+
         public long Id { get; set; }
         public string Name { get; set; }
         public string Currency { get; set; }
--- IndicesReferentialApi/Program.cs
+++ IndicesReferentialApi/Program.cs
@@ -1,8 +1,13 @@
-﻿var builder = WebApplication.CreateBuilder(args);
+﻿using IndicesReferentialApi.Models;
+using Microsoft.EntityFrameworkCore;
+
+var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
 builder.Services.AddControllers();
+builder.Services.AddDbContext<FinancialIndexContext>(
+    opt => opt.UseInMemoryDatabase("FinancialIndexList"));
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
